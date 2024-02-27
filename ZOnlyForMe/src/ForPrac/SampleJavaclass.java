8d92690edad2b5faed2a967cf3f25877c8aeec2a
bajradhyan <37986460+bajradhyan@users.noreply.github.com>
2024-02-27 12:11:37 -0500

    SeleniumPracticeProjLoadding

---
+++ ZOnlyForMe/src/ForPrac/SampleJavaclass.java
@@ -0,0 +1,10 @@
+package ForPrac;
+
+public class SampleJavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is java class");
+	}
+
+}
